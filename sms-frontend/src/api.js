1f7e46444e6e025568d70e086bd7800be8ef09bb
mahitha005 <mahitha2005.mahi@gmail.com>
2026-03-11 19:56:10 +0530

    fix backend api url

--- sms-frontend/src/api.js
+++ sms-frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://service-monitoring-system.onrender.com/",
   headers: {
     "Content-Type": "application/json"
   }
