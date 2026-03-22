ddcf05a937c329dfae2e3f947aed0e7f11761074
mahitha005 <mahitha2005.mahi@gmail.com>
2026-03-22 18:13:25 +0530

    backend deployment ready

--- sms-frontend/src/api.js
+++ sms-frontend/src/api.js
@@ -1,20 +1,17 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080",
   headers: {
     "Content-Type": "application/json"
   }
 });
 
 api.interceptors.request.use((config) => {
-
   const token = localStorage.getItem("token");
-
   if (token && !config.url.startsWith("/auth")) {
     config.headers.Authorization = `Bearer ${token}`;
   }
-
   return config;
 });
 
